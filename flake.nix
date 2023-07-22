ff13db05d8443fa03ca67f78abf7a8f9687c2689
sandmuel <115949508+sandmuel@users.noreply.github.com>
2023-07-21 19:30:29 -0700

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Minigui - A minimal window manager based GUI.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: {
+      Minigui = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            # Enable the display server
+            services.xserver.enable = {
+              enable = true;
+            };
+
+            # Enable the display manager
+            services.xserver.displayManager.lightdm = {
+              enable = true;
+            }
+
+            # Enable the window manager
+            services.xserver.windowManager.bspwm = {
+              enable = true;
+            };
+
+            services.picom = {
+              enable = true;
+            }
+          }
+        ];
+      };
+    };
+  };
+}
