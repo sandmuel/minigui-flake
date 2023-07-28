5e212ceac75d72e6d99e3ce7a2e706ab0b121197
sandmuel <115949508+sandmuel@users.noreply.github.com>
2023-07-27 21:21:08 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
-{ config, lib, pkgs, ... }:
 {
   description = "Minigui - A minimal window manager based GUI.";
 
-  services.xserver = {
-  enable = true;
-  windowManager.bspwm.enable = true;
-  displayManager.defaultSession = "none+bspwm";
+  outputs = { self, nixpkgs, services }:
+  {
+    services.xserver = 
+    {
+      enable = true;
+      windowManager.bspwm.enable = true;
+      displayManager.defaultSession = "none+bspwm";
+    };
+  };
 }
