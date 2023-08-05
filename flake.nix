6f09ca30da0fa7061d12fa467d4d3a34d8bbda14
sandmuel <115949508+sandmuel@users.noreply.github.com>
2023-08-05 16:01:50 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs;
   
   outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.fnord = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.hostname = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
       modules = [ ./configuration.nix ];
