ddeb002ec90379f60bd852b5939c2deb29c38e5a
sandmuel <115949508+sandmuel@users.noreply.github.com>
2023-08-05 16:00:43 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,31 +1,11 @@
 {
-  description = "Minigui - A minimal window manager based GUI.";
-
-  # Define flake inputs
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-  };
-
-  # Define flake outputs
-  outputs = { self, nixpkgs }: 
-  {
-
-    ###################
-    # Set up services #
-    ###################
-
-    services.xserver = {
-      enable = true;
-      windowManager.bspwm.enable = true;
-      displayManager.defaultSession = "none+bspwm";
-      displayManager.lightdm.enable = true;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  
+  outputs = { self, nixpkgs, ... }@attrs: {
+    nixosConfigurations.fnord = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [ ./configuration.nix ];
     };
-
-    #####################
-    # Add some packages #
-    #####################
-
-    packages = [ nixpkgs.alacritty nixpkgs.polybar ];    
-
   };
 }
