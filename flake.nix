53cf8cb4e185f1dc9a046c841b35ac39a76b0d69
sandmuel <115949508+sandmuel@users.noreply.github.com>
2023-08-05 17:07:44 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A minimal desktop environment."
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { nixpkgs, ... }:
