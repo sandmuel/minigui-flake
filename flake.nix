d47a7e796828d9870f50cf5dbd6b4c7de86b712c
sandmuel <115949508+sandmuel@users.noreply.github.com>
2023-08-05 17:00:53 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,28 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.hostname = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [ ./configuration.nix ];
+  description = "A minimal desktop environment."
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... }:
+
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+
+    lib = nixpkgs.lib;
+
+  in {
+    nixosConfigurations = {
+      hostname = lib.nixosSystem {
+        inherit system;
+        modules = [ /etc/nixos/configuration.nix ];
+      };
     };
   };
 }
